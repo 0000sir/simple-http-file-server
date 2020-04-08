a09bff64d99828242300cf8782483b07d1e0805a
0000sir <0000sir@gmail.com>
2020-04-08 17:46:25 +0800

    feat: creat uploads directory if not exist

--- server.sh
+++ server.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-cd /opt/http-server/uploads && python3 /opt/http-server/server.py --access_config /opt/http-server/config/users.json --log_headers --log /var/log/http.log --threads=2 80
+if [ -d uploads ];then
+    mkdir -p /opt/http-server/uploads
+end
+cd /opt/http-server/uploads
+
+python3 /opt/http-server/server.py --access_config /opt/http-server/config/users.json --log_headers --log /var/log/http.log --threads=2 80
